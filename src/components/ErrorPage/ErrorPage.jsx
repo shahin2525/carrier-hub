772023ab266d7721c49eeb4096f1b2a24d565c5c
shahin2525 <shahin451999@gmail.com>
2024-07-10 20:55:06 -0700

    add nav

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 const ErrorPage = () => {
   return (
-    <div>
-      <h1 className="text-center">Oops</h1>
+    <div className="text-center">
+      <h1>Oops</h1>
+      <Link to="/">Go Home</Link>
     </div>
   );
 };
